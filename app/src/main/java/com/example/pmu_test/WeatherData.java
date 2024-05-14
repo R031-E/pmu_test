8be31168451d00926bb7fc24e6472e366d8be733
nikit <nikitamaz@hotmail.com>
2024-05-14 20:10:55 +0300

    added shake to update

--- app/src/main/java/com/example/pmu_test/WeatherData.java
+++ app/src/main/java/com/example/pmu_test/WeatherData.java
@@ -1,6 +1,11 @@
 package com.example.pmu_test;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,6 +29,7 @@ import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class WeatherData extends AppCompatActivity {
     private String apiKey = "&appid=d77b3f0e8e7ad3d429f208e537c5b5b9";
@@ -35,6 +41,12 @@ public class WeatherData extends AppCompatActivity {
 
     private String iconIdField;
 
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private SensorEventListener sensorListener;
+    private static final int MIN_TIME_BETWEEN_SHAKES_MILLISECS = 2000;
+    private long mLastShakeTime;
+
     private GestureDetector gestureDetector;
 
     private Boolean isOutdated;
@@ -45,6 +57,7 @@ public class WeatherData extends AppCompatActivity {
         RelativeLayout buttonContainer = findViewById(R.id.button_container);
         Button cancelButton = findViewById(R.id.cancel_button);
         Button addButton = findViewById(R.id.add_button);
+        TextView shakeHint = findViewById(R.id.shake_to_update_hint);
         // Получаем данные из MainActivity
         int id = getIntent().getIntExtra("id", -2);
         String city = getIntent().getStringExtra("city");
@@ -64,6 +77,7 @@ public class WeatherData extends AppCompatActivity {
                 final int SWIPE_MIN_DISTANCE = 120;
                 final int SWIPE_THRESHOLD_VELOCITY = 200;
                 if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Log.d("WeatherData", "Swipe right");
                     if (isOutdated && timestamp == 0) {
                         setResult(RESULT_CANCELED);
                         finish();
@@ -89,6 +103,7 @@ public class WeatherData extends AppCompatActivity {
                     }
                     return true;
                 }
+                Log.d("WeatherData", "Swipe left");
                 return false;
             }
         });
@@ -119,6 +134,36 @@ public class WeatherData extends AppCompatActivity {
                 finish();
             }
         });
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                long currentTime = System.currentTimeMillis();
+                if ((currentTime - mLastShakeTime) > MIN_TIME_BETWEEN_SHAKES_MILLISECS) {
+                    float x = event.values[0];
+                    float y = event.values[1];
+                    float z = event.values[2];
+
+                    double acceleration = Math.sqrt(x * x + y * y + z * z);
+
+                    if (acceleration > 12) {
+                        mLastShakeTime = currentTime;
+                        shakeHint.setText(R.string.data_updated_with_shake_hint);
+                        getWeatherData(city, url);
+                        isOutdated = true;
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                // Не обязательно реализовывать
+            }
+        };
+
+        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         cityTextView = findViewById(R.id.textViewCity);
         weatherImageView = findViewById(R.id.imageViewWeatherIcon);
         temperatureTextView = findViewById(R.id.textViewTemperature);
@@ -185,6 +230,7 @@ public class WeatherData extends AppCompatActivity {
                     windTextView.setText(windString);
                     pressureTextView.setText(pressureString);
                     visibilityTextView.setText(visibilityString);
+                    Log.d("WeatherData", "Weather data updated");
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -199,5 +245,11 @@ public class WeatherData extends AppCompatActivity {
         });
 
         queue.add(request);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(sensorListener);
     }
 }
